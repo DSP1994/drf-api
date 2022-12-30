0aabc80bdf3fb9958d6c3ca280e287ce9257f724
David <parrydavid@live.co.uk>
2022-12-30 15:53:37 +0000

    added image filter and validation

--- posts/serializers.py
+++ posts/serializers.py
@@ -7,6 +7,21 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size is larger than 2MB!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        return value
+
     def get_is_owner(self, obj):
         request = self.context.get('request')
         if request and request.user:
@@ -18,4 +33,6 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'owner', 'is_owner', 'profile_id',
-                  'profile_image', 'title', 'description']
+                  'profile_image', 'created_at', 'updated_at',
+                  'title', 'content', 'image', 'image_filter',
+                  'description']
