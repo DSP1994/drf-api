8b3c1f669652d8afd34a125bd260dd2892ff94c3
David <parrydavid@live.co.uk>
2023-01-11 09:20:39 +0000

    updated comments time

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
 
@@ -7,11 +8,19 @@ class CommentSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image_url')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     class Meta:
         model = Comment
         fields = ['id', 'owner', 'is_owner', 'profile_id', 'profile_image',
                   'post', 'created_at', 'updated_at', 'content']
+    
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+    
+    def get_updated_at(self, obj):
+        return naturaltime(obj.created_at)
 
     def get_is_owner(self, obj):
         request = self.context.get("request")
